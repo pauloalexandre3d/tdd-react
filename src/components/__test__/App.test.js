9141f7bfbdb102169fc739ba692fa12e51b5bd26
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-01 08:31:21 +0200

    setup: Adding setup redux

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,8 +4,13 @@ import { shallow } from 'enzyme';
 import CommentBox from '../CommentBox';
 import CommentList from '../CommentList';
 
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<App />);
+});
+
 it('Shows a comment box', () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
