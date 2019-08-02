c6ff6c8b5c379ee67bd657fc1e3b528b6f57d4f4
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-02 23:52:13 +0200

    test: Adding integration tests.

---
+++ src/__tests__/integration.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from 'Root';
+import App from 'components/App';
+
+it('can fetch a list of comments and display them', () => {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
+  wrapped.find('.fetch-comments').simulate('click');
+  
+  expect(wrapped.find('li').length).toEqual(500);
+});
