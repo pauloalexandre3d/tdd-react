88da6aae6cf8cb4b90bbd78ab6fc5aa65923f111
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-03 01:31:13 +0200

    feature: Adding react router

--- src/__tests__/integration.test.js
+++ src/__tests__/integration.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Root from 'Root';
 import App from 'components/App';
+import moxios from 'moxios';
+import { watchFile } from 'fs';
 
-it('can fetch a list of comments and display them', () => {
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched 1' }, { name: 'Fetched 2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', done => {
   const wrapped = mount(
     <Root>
       <App />
@@ -11,6 +25,11 @@ it('can fetch a list of comments and display them', () => {
   );
 
   wrapped.find('.fetch-comments').simulate('click');
-  
-  expect(wrapped.find('li').length).toEqual(500);
+
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find('li').length).toEqual(2);
+    done();
+    wrapped.unmount();
+  });
 });
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'Root';
 import App from './components/App';
+import { Router, BrowserRoute } from 'react-router-dom';
 
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRoute>
+      <Router path="/" component={App} />
+    </BrowserRoute>
   </Root>,
   document.querySelector('#root')
 );
