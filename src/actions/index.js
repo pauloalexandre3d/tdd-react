634f2e1097f96f64c530ff6fc275219a1087b4b7
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-02 21:21:46 +0200

    feature: Adding fetching a remote resource

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
 import { SAVE_COMMENT } from 'actions/types';
+import { FETCH_COMMENTS } from 'actions/types';
+import axios from 'axios';
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  };
+}
+
+export function fetchComments() {
+  const response = axios.get('https://jsonplaceholder.typicode.com/comments');
+
+  return {
+    type: FETCH_COMMENTS,
+    payload: response
   };
 }
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -9,24 +9,30 @@ class CommentBox extends Component {
     this.setState({ comment: event.target.value });
   };
 
-  handleSubmit =event => {
+  handleSubmit = event => {
     event.preventDefault();
 
     this.props.saveComment(this.state.comment);
-    this.setState({comment: ''});
+    this.setState({ comment: '' });
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add a Comment</h4>
-        <textarea onChange={this.handleChange} value={this.state.comment} />
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
     );
   }
 }
 
-export default connect(null, actions)( CommentBox);
+export default connect(
+  null,
+  actions
+)(CommentBox);
